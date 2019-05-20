4d3c4a237216501fe5f2e03ba98da8d59e47f134
Hoa <huynhtehoa@gmail.com>
2019-05-20 18:25:16 +0700

    first commit

--- game.js
+++ game.js
@@ -9,22 +9,26 @@ Here, we create and add our "canvas" to the page.
 We also load all of our images. 
 */
 
-
+// Create canvas
 let canvas;
 let ctx;
 
-canvas = document.createElement("canvas");
+canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
-document.body.appendChild(canvas);
+canvas.width = 800;
+canvas.height = 600;
 
-let bgReady, heroReady, monsterReady;
-let bgImage, heroImage, monsterImage;
+let bgReady, catReady, sushiReady, cageReady, ramenReady;
+let bgImage, catImage, sushiImage, cageImage, ramenImage;
 
 let startTime = Date.now();
-const SECONDS_PER_ROUND = 30;
+let SECONDS_PER_ROUND = 3;
 let elapsedTime = 0;
+let remainingTime;
+
+let score = 0;
+let round = 0;
+let totalScore = 0;
 
 function loadImages() {
   bgImage = new Image();
@@ -32,37 +36,65 @@ function loadImages() {
     // show the background image
     bgReady = true;
   };
-  bgImage.src = "images/background.png";
-  heroImage = new Image();
-  heroImage.onload = function () {
-    // show the hero image
-    heroReady = true;
+  bgImage.src = "images/sushi-background.jpg";
+  catImage = new Image();
+  catImage.onload = function () {
+    // show the cat image
+    catReady = true;
   };
-  heroImage.src = "images/hero.png";
+  catImage.src = "images/meme-cat-face.png";
 
-  monsterImage = new Image();
-  monsterImage.onload = function () {
-    // show the monster image
-    monsterReady = true;
+  sushiImage = new Image();
+  sushiImage.onload = function () {
+    // show the sushi image
+    sushiReady = true;
   };
-  monsterImage.src = "images/monster.png";
+  sushiImage.src = "images/kawaii-sushi.png";
+
+  cageImage = new Image();
+  cageImage.onload = function () {
+    // show the cage image
+    cageReady = true;
+  };
+  cageImage.src = "images/cage.png";
+
+  ramenImage = new Image();
+  ramenImage.onload = function () {
+    // show the ramen image
+    ramenReady = true;
+  };
+  ramenImage.src = "images/ramen.png";
 }
 
 /** 
  * Setting up our characters.
  * 
- * Note that heroX represents the X position of our hero.
- * heroY represents the Y position.
- * We'll need these values to know where to "draw" the hero.
+ * Note that catX represents the X position of our cat.
+ * catY represents the Y position.
+ * We'll need these values to know where to "draw" the cat.
  * 
- * The same applies to the monster.
+ * The same applies to the sushi.
  */
 
-let heroX = canvas.width / 2;
-let heroY = canvas.height / 2;
+let catX = canvas.width / 2;
+let catY = canvas.height / 2;
 
-let monsterX = 100;
-let monsterY = 100;
+let sushiX = Math.round(Math.random() * canvas.width);
+let sushiY = Math.round(Math.random() * canvas.height);
+
+let cageX = Math.round(Math.random() * canvas.width);
+let cageY = Math.round(Math.random() * canvas.height);
+
+let ramenX = Math.round(Math.random() * canvas.width);
+let ramenY = Math.round(Math.random() * canvas.height);
+
+let sushiDirectionX = 1;
+let sushiDirectionY = 1;
+let hasntRestarted  = true;
+
+let cageDirectionX = 1;
+let cageDirectionY = 1;
+
 
 /** 
  * Keyboard Listeners
@@ -86,7 +118,7 @@ function setupKeyboardListeners() {
 
 /**
  *  Update game objects - change player position based on key pressed
- *  and check to see if the monster has been caught!
+ *  and check to see if the sushi has been caught!
  *  
  *  If you change the value of 5, the player will move at a different rate.
  */
@@ -94,32 +126,115 @@ let update = function () {
   // Update the time.
   elapsedTime = Math.floor((Date.now() - startTime) / 1000);
 
-
   if (38 in keysDown) { // Player is holding up key
-    heroY -= 5;
+    catY -= 5;
   }
   if (40 in keysDown) { // Player is holding down key
-    heroY += 5;
+    catY += 5;
   }
   if (37 in keysDown) { // Player is holding left key
-    heroX -= 5;
+    catX -= 5;
   }
   if (39 in keysDown) { // Player is holding right key
-    heroX += 5;
+    catX += 5;
   }
 
-  // Check if player and monster collided. Our images
-  // are about 32 pixels big.
+  // Cat moves through borders
+  if (catX > canvas.width - 32) {
+    catX -= canvas.width;
+  } else if (catX < 0) {
+    catX += canvas.width;
+  }
+
+  if (catY > canvas.height - 32) {
+    catY -= canvas.height;
+  } else if (catY < 0) {
+    catY += canvas.height;
+  }
+
+  //Sushi bounces back
+  sushiX += sushiDirectionX * 2;
+  sushiY += sushiDirectionY * 2;
+
+  if (sushiX > canvas.width - 32 || sushiX < 0) {
+    sushiDirectionX = -sushiDirectionX;
+  }
+
+  if (sushiY > canvas.height - 32 || sushiY < 0) {
+    sushiDirectionY = -sushiDirectionY;
+  }
+
+  //Cage bounces back
+  cageX += cageDirectionX * (round * 1);
+  cageY += cageDirectionY * (round * 1);
+
+  if (cageX > canvas.width - 32 || cageX < 0) {
+    cageDirectionX = -cageDirectionX;
+  }
+
+  if (cageY > canvas.height - 32 || cageY < 0) {
+    cageDirectionY = -cageDirectionY;
+  }
+
+
+  // Check if player and sushi collided.
   if (
-    heroX <= (monsterX + 32)
-    && monsterX <= (heroX + 32)
-    && heroY <= (monsterY + 32)
-    && monsterY <= (heroY + 32)
+    catX <= (sushiX + 50)
+    && sushiX <= (catX + 50)
+    && catY <= (sushiY + 50)
+    && sushiY <= (catY + 50)
   ) {
-    // Pick a new location for the monster.
-    // Note: Change this to place the monster at a new, random location.
-    monsterX = monsterX + 50;
-    monsterY = monsterY + 70;
+    // Pick a new location for the sushi.
+    // Note: Change this to place the sushi at a new, random location.
+    sushiX = Math.round(Math.random() * canvas.width);
+    sushiY = Math.round(Math.random() * canvas.height);
+    ramenX = Math.round(Math.random() * canvas.width);
+    ramenY = Math.round(Math.random() * canvas.height);
+
+    // Increase score by 1 for every collision
+    totalScore++;
+    score++;
+
+    // Increase time by 2s for every collision
+    SECONDS_PER_ROUND += 2;
+  };
+
+  if (
+    catX <= (cageX + 50)
+    && cageX <= (catX + 50)
+    && catY <= (cageY + 50)
+    && cageY <= (catY + 50)
+  ) {
+    // Pick a new location for the cage.
+    // Note: Change this to place the cage at a new, random location.
+    cageX = Math.round(Math.random() * canvas.width);
+    cageY = Math.round(Math.random() * canvas.height);
+
+    // Lose if cat and cage collided
+    alert("My cat get caught 🙀. Auto restarting...");
+    reset();
+  }
+
+  if (
+    catX <= (ramenX + 30)
+    && ramenX <= (catX + 30)
+    && catY <= (ramenY + 30)
+    && ramenY <= (catY + 30)
+  ) {
+    // Pick a new location for the ramen.
+    // Note: Change this to place the ramen at a new, random location.
+    ramenX = Math.round(Math.random() * canvas.width);
+    ramenY = Math.round(Math.random() * canvas.height);
+
+    // Lose if cat and ramen collided
+    alert("My cat needs to 💩. Auto restarting...");
+    reset();
+  }
+
+  // Everytime score reaches multiples 5 => round++ and add previous score to totalScore;
+  if (score == 3) {
+    round++;
+    score = 0;
   }
 };
 
@@ -127,25 +242,72 @@ let update = function () {
  * This function, render, runs as often as possible.
  */
 var render = function () {
+  remainingTime = SECONDS_PER_ROUND - elapsedTime;
+
+  // Reset if remaining time = 0;
+  if (remainingTime === 0 && hasntRestarted === true) {
+    hasntRestarted = false;
+    alert("Out of time 😿. Auto restarting...");
+    return reset();
+  }
+
   if (bgReady) {
     ctx.drawImage(bgImage, 0, 0);
   }
-  if (heroReady) {
-    ctx.drawImage(heroImage, heroX, heroY);
+  if (catReady) {
+    ctx.drawImage(catImage, catX, catY);
   }
-  if (monsterReady) {
-    ctx.drawImage(monsterImage, monsterX, monsterY);
+  if (sushiReady) {
+    ctx.drawImage(sushiImage, sushiX, sushiY);
   }
-  ctx.fillText(`Seconds Remaining: ${SECONDS_PER_ROUND - elapsedTime}`, 20, 100);
+  if (cageReady) {
+    ctx.drawImage(cageImage, cageX, cageY);
+  }
+  if (ramenReady) {
+    ctx.drawImage(ramenImage, ramenX, ramenY);
+  }
+  document.getElementById("remaining-time").innerHTML = `${remainingTime}`;
+  document.getElementById("total-score").innerHTML = `${totalScore}`;
+  document.getElementById("round").innerHTML = `${round}`;
+};
+
+// Reset game
+var reset = function () {
+  window.location.reload();
+}
+
+// Music control
+var myAudio = document.getElementById("audio");
+var isPlaying = true;
+
+myAudio.autoplay = true;
+
+function togglePlay() {
+  if (isPlaying) {
+    myAudio.pause();
+    document.getElementById("audio-button").innerHTML = "🎼 Start";
+    document.getElementById("audio-button").className = "btn btn-success";
+
+  } else {
+    myAudio.play();
+    document.getElementById("audio-button").innerHTML = "Pause 🎹";
+    document.getElementById("audio-button").className = "btn btn-warning text-white";
+  }
+};
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
 };
 
 /**
  * The main game loop. Most every game will have two distinct parts:
- * update (updates the state of the game, in this case our hero and monster)
+ * update (updates the state of the game, in this case our cat and sushi)
  * render (based on the state of our game, draw the right things)
  */
 var main = function () {
-  update(); 
+  update();
   render();
   // Request to do this again ASAP. This is a special method
   // for web browsers. 
@@ -161,3 +323,4 @@ requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
 loadImages();
 setupKeyboardListeners();
 main();
+
